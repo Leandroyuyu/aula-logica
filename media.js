c3b367645091b69c32b68ab32533429b57081e96
leandroyuyu <leandro.7549@colegioprogresso.g12.br>
2026-03-09 12:12:50 -0300

    Alterei media

---
+++ media.js
@@ -0,0 +1,7 @@
+function somaNumeros(a,b){
+    return (a + b) /2
+}
+
+const numero=(8,10)
+
+console.log (numero)
--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const numero1= 8
-const numero2= 12
-
-function somaNumeros(){
-    console.log("cheguei aqui")
+function somaNumeros(a,b){
+    return a + b
 }
 
-somaNumeros()
+const Soma1 = somaNumeros (8,177)
+const Soma2 = somaNumeros (8,190)
+const Soma3 = somaNumeros (8,66)
+const Soma4 = somaNumeros (8,14)
+
+console.log(Soma1)
+console.log(Soma2)
+console.log(Soma3)
+console.log(Soma4)
